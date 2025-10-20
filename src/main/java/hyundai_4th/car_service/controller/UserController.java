64ed5c086b93edb17ee23d1256ef66bd83077279
BuSeong <qkrqntjd12394@gmail.com>
2025-10-20 09:16:26 +0900

    add ignore file type

--- src/main/java/hyundai_4th/car_service/controller/UserController.java
+++ src/main/java/hyundai_4th/car_service/controller/UserController.java
@@ -1,4 +1,17 @@
 package hyundai_4th.car_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/")
+    public String home() {
+        return "Car Service API is running!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 }
