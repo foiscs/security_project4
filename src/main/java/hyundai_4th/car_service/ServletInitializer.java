d104b2bf7cda3c461c9d3a95ab2c6377f16afb7d
nyangmyang <koyehyeon2001@gmail.com>
2025-10-22 14:30:39 +0900

    war파일 생성

---
+++ src/main/java/hyundai_4th/car_service/ServletInitializer.java
@@ -0,0 +1,11 @@
+package hyundai_4th.car_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CarServiceApplication.class);
+    }
+}
